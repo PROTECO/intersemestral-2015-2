ad222b23249c79823a365e90505ecb5616690a6e
Mark <mcmarco2@gmail.com>
2015-01-19 11:15:58 -0600

    ejercicio2 input change text

---
+++ JavaScript/js/javascript-basico.js
@@ -0,0 +1,211 @@
+function(){
+  var a = 2;
+  return alert(a+3);
+}
+//Función anónima
+
+
+
+
+/*
+var variable = "Hola ";
+var variable2 = "Mundo";
+
+
+
+//alert(variable+variable2);
+
+//variable = 232;
+
+//alert(variable+variable2);
+
+
+//var arreglo = ["Lunes","Martes", "Miercoles"]
+
+//alert("Hoy es "arreglo[0]);
+//alert(noinicializada);
+
+
+//var objetos = [variable,variable2]
+
+//alert(objetos[0]+objetos[1])
+
+//var nombreUsuario = prompt("Ingresa tu nombre","Anonimo")
+//alert("Bienvenido "+nombreUsuario);
+
+//var x = 3.1415900000303003030320230
+//var y = 34
+
+//var z = 123e5  // 12,300,000
+//var w = 123e-5 // .00123
+
+//var a = x.toExponential();
+//alert(7+a);
+
+/*
+  + Suma
+  - resta
+  * Multiplicacion
+  / division
+  % Modulo
+  ++ Incremento
+  -- Decremento
+*/
+/*
+
+var resultado = x+y;
+resultado = x-y;
+
+variable += variable2;
+
+
+  Operadores de Comparacion
+  == iguales
+  != no iguales
+  === Valor igual ,tipo de dato
+  !== valor no igual, tipo de dato no igual
+  > Mayor
+  < Menor
+  >= Mayor o igual
+  <= Menor Igual
+*/
+/*
+
+x="hola";
+//alert(parseInt(x)+1)
+
+z = 848438;
+z = z.toString();
+
+function muestraNombre () {
+  var nombre = prompt("Cual es tu nombre");
+  alert("Tu nobre es: "+ nombre);
+}
+
+
+function suma(a,b){
+  //var a = pedirDatos();
+  //var b = pedirDatos();
+  alert(a+b)
+}
+
+function resta (){
+  var a = pedirDatos();
+  var b = pedirDatos();
+  alert(a-b)
+}
+
+function pedirDatos () {
+  c = prompt("Dame un numero")
+  return parseInt(c);
+}
+
+suma(2,5);
+
+
+function contador(){
+  var nombres = [" Marco",
+                 " Jared",
+                 " Jonhy",
+                 " Charly",
+                 " Martin",
+                 " Faustino",
+                 " Oscar",
+                 " Mariana"
+                ];
+*/
+  // alert(nombres.length);
+  // var x = 0;
+  // console.log("<table>")
+  
+  // while(x < 8){
+  //   console.log("<tr><td>")
+  //   console.log(nombres[x]);
+  //   console.log("</tr></td>")
+
+  //   x++;
+  // }
+  
+  // console.log("</table>")
+  // console.log("Salimos del ciclo")
+/*
+
+  var saludo = " hola ";
+
+  // console.log(saludo += nombres[0]);
+  // console.log(saludo += nombres[1]);
+  // console.log(saludo += nombres[2]);
+  // console.log(saludo += nombres[3]);
+  // console.log(saludo += nombres[4]);
+  // console.log(saludo += nombres[5]);
+
+  for(i = 0; i<nombres.length; i++){
+      console.log(saludo += nombres[i]);
+  }
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var x = 1;
+  // x= 2;
+  // x= 3;
+  // x= 4;
+  // var saludo = "Hola Faustino";
+  // alert(saludo);
+  // saludo = "Hola Faustino!";
+  // saludo = "Hola Faustino!!";
+  // saludo = "Hola Faustino!!!";
+  // saludo = "Hola Faustino!!!!";
+  // saludo = "Hola Faustino!!!!!";
+  // alert(saludo);
+
+  // var sexo = prompt("Ingresa tu sexo");
+  // var personaje = prompt("Cual es tu nombre?");
+
+
+  // if(sexo == "masculino"){
+    
+  //   if(personaje == "Oscar"){
+  //     alert("Hola Oscar");
+  //   }else{
+  //     alert("Hola "+ personaje + sexo)
+
+  //   }
+  
+  // }else{
+  //   alert("Sexo femenino");
+
+  //   if(personaje == "Mary"){
+  //     alert("Hola" + personaje + sexo);
+  //   }else{
+  //     alert("No eres Mary" + sexo);
+  //   }
+
+
+  // }
+
+
+
+
+
+//}
+//contador();
+
+
+
+
+
--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,104 +1,34 @@
+function cambiarTexto(){
 
-var variable = "Hola ";
-var variable2 = "Mundo";
+// alert(document.getElementById("saludo").innerHTML);
+// alert(document.getElementsByTagName("div")[0]);
+// alert(document.getElementsByClassName("navbar")[0]);
+// && AND
+// || OR
 
-
-
-//alert(variable+variable2);
-
-variable = 232;
-
-//alert(variable+variable2);
-/**/
-
-var arreglo = ["Lunes","Martes", "Miercoles"]
-
-//alert("Hoy es "arreglo[0]);
-//alert(noinicializada);
+var correo = document.getElementById("input_name").value;
+var pass = document.getElementById("input_pass").value;
 
 
-var objetos = [variable,variable2]
-
-//alert(objetos[0]+objetos[1])
-
-//var nombreUsuario = prompt("Ingresa tu nombre","Anonimo")
-//alert("Bienvenido "+nombreUsuario);
-
-var x = 3.1415900000303003030320230
-var y = 34
-
-var z = 123e5  // 12,300,000
-var w = 123e-5 // .00123
-
-var a = x.toExponential();
-//alert(7+a);
-
-/*
-  + Suma
-  - resta
-  * Multiplicacion
-  / division
-  % Modulo
-  ++ Incremento
-  -- Decremento
-*/
-
-var resultado = x+y;
-resultado = x-y;
-
-variable += variable2;
-
-/*
-  Operadores de Comparacion
-  == iguales
-  != no iguales
-  === Valor igual ,tipo de dato
-  !== valor no igual, tipo de dato no igual
-  > Mayor
-  < Menor
-  >= Mayor o igual
-  <= Menor Igual
-*/
-
-x="hola";
-//alert(parseInt(x)+1)
-
-z = 848438;
-z = z.toString();
-
-function muestraNombre () {
-  var nombre = prompt("Cual es tu nombre");
-  alert("Tu nobre es: "+ nombre);
+if(correo == "jared@gmail.com" && pass == "123"){
+  document.getElementById("saludo").innerHTML = "Hola Jared";
+}else{
+  document.getElementById("saludo").innerHTML = "Prohibido";
 }
 
 
-function suma(a,b){
-  /*var a = pedirDatos();
-  var b = pedirDatos();*/
-  alert(a+b)
+
+
+// onchange = cuando un elemento HTML ha cambiado
+// onclick = cuando el usuario da click en el elemento
+
+// onmouseover = cuando el usuario pasa el cursor sobre el elemento
+// onmouseout = cuando el cursor sale del elemento
+
+// onkeydown = cuando el usuario aprieta una tecla
+//   onkeypress = el usuario presiona
+//   onkeyup = el usuario suelta
+
+// onload = cuando el navegador termina de cargar la pagina
+
 }
-
-function resta (){
-  var a = pedirDatos();
-  var b = pedirDatos();
-  alert(a-b)
-}
-
-function pedirDatos () {
-  c = prompt("Dame un numero")
-  return parseInt(c);
-}
-
-suma(2,5);
-
-
-
-
-
-
-
-
-
-
-
-
