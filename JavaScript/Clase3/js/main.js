180a239cd6bdc8209c458c8e60c8af9aff419715
Mark <mcmarco2@gmail.com>
2015-01-21 12:09:14 -0600

    jQuery2

--- JavaScript/Clase3/js/main.js
+++ JavaScript/Clase3/js/main.js
@@ -1,15 +1,14 @@
-function agregar(){
-  var tipo = prompt("Ingresa el tipo de input");
-  // Obtenemos el tipo de input
-  var input = document.createElement("input");
-//Creamos un elemento de input
-  input.type= tipo;
-  //Al atributo type le asignamos el valor que ingreso el usuario
+$(document).ready(principal);
 
-  var div = document.getElementById("nombre");
-  // Encontramos el contenedor donde vamos a insertar nuestro input
-  div.appendChild(input);
-  // Insertamos el elemento "hijo" a la lista
 
+function principal(){
+
+
+  $(".spoiler").css("background", "red");
+
+
+  $("span").text("hola");
+
+  $("span").css("color", "green");
 
 }
